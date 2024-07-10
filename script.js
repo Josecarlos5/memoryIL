45394a5f093015b1d1bd1c70ab831758520d64a4
Josecarlos5 <dsilvacouto5@gmail.com>
2024-07-11 00:23:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let chosenCards = [];
     let chosenCardIds = [];
     let cardsWon = [];
-    let lockBoard = false;
 
     function createBoard() {
         cardArray.forEach((card, index) => {
@@ -54,23 +53,18 @@ document.addEventListener("DOMContentLoaded", () => {
             cardElement.setAttribute('data-id', index);
             cardElement.addEventListener('click', flipCard);
             const cardImg = document.createElement('img');
-            cardImg.setAttribute('src', 'images/blank.png'); // Placeholder image
+            cardImg.setAttribute('src', card.img);
             cardElement.appendChild(cardImg);
             gameBoard.appendChild(cardElement);
         });
     }
 
     function flipCard() {
-        if (lockBoard) return;
         const cardId = this.getAttribute('data-id');
-        if (chosenCardIds.includes(cardId)) return;
-
-        this.querySelector('img').setAttribute('src', cardArray[cardId].img);
         chosenCards.push(cardArray[cardId].name);
         chosenCardIds.push(cardId);
-
+        this.classList.add('flipped');
         if (chosenCards.length === 2) {
-            lockBoard = true;
             setTimeout(checkForMatch, 500);
         }
     }
@@ -85,13 +79,12 @@ document.addEventListener("DOMContentLoaded", () => {
             cards[optionTwoId].removeEventListener('click', flipCard);
             cardsWon.push(chosenCards);
         } else {
-            cards[optionOneId].querySelector('img').setAttribute('src', 'images/blank.png');
-            cards[optionTwoId].querySelector('img').setAttribute('src', 'images/blank.png');
+            cards[optionOneId].classList.remove('flipped');
+            cards[optionTwoId].classList.remove('flipped');
         }
 
         chosenCards = [];
         chosenCardIds = [];
-        lockBoard = false;
 
         if (cardsWon.length === cardArray.length / 2) {
             alert('Congratulations! You found all the matches!');
